a4338b0f341359e03ef5cf9e3ce3f22d4ec1c0d1
PacorrOz <59944099+PacorrOz@users.noreply.github.com>
2020-11-07 03:02:29 -0600

    Agregada funcionn para exportar datos a txt

---
+++ Exportar.py
@@ -0,0 +1,10 @@
+from Conexion import Conexion
+
+class Exportar:
+    def Exportar(self):
+        Conexion.mycursor.execute('SELECT * FROM Alumno')
+
+        f = open("datos.txt", "w+")
+        for row in Conexion.mycursor:
+            f.write(str(row))        
+        f.close()
